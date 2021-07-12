7cca1192261ed1eef23459c6ac6acc1cbb6bac39
tissy01 <musicraunjak22@gmail.com>
2021-07-12 14:44:58 +0200

    Main Screen und GUI der App Designs und wechsel zwischen den Screens (Vorerst nur Settings)

--- app/src/main/java/com/uni/foodfindar/MainActivity.kt
+++ app/src/main/java/com/uni/foodfindar/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.uni.foodfindar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var setting:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setting = findViewById(R.id.settings)
+
+        setting.setOnClickListener{
+            val intent = Intent(this, Settings::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
