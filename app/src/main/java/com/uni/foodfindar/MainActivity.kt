b68ca49850897a500b0c17c733c8339d012d7d33
tissy01 <musicraunjak22@gmail.com>
2021-07-13 11:56:56 +0200

    Get Location Button implementiert, was noch fehlt ist, die longitude und latitude global zu speichern

--- app/src/main/java/com/uni/foodfindar/MainActivity.kt
+++ app/src/main/java/com/uni/foodfindar/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.uni.foodfindar
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var setting:Button
     lateinit var menu:Button
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,10 @@ class MainActivity : AppCompatActivity() {
 
         setting = findViewById(R.id.settings)
         menu = findViewById(R.id.menu)
+
+
+
+
 
         setting.setOnClickListener{
 
@@ -35,4 +46,5 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
 }
--- app/src/main/java/com/uni/foodfindar/Settings.kt
+++ app/src/main/java/com/uni/foodfindar/Settings.kt
@@ -1,14 +1,23 @@
 package com.uni.foodfindar
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
 
 class Settings : AppCompatActivity() {
     lateinit var setting: Button
+    lateinit var location: Button
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onBackPressed() {
         super.onBackPressed()
@@ -20,10 +29,35 @@ class Settings : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         setting = findViewById(R.id.settings)
+        location = findViewById(R.id.standort)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         setting.setOnClickListener{
             finish()
             overridePendingTransition(0, 0)
         }
+
+        findViewById<Button>(R.id.standort).setOnClickListener(){
+            fetchLocation()
+        }
+
+    }
+   private fun fetchLocation(){
+        val task = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION )
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),101)
+            return
+        }
+
+        task.addOnSuccessListener {
+            if(it != null){
+                Toast.makeText(applicationContext, "${it.latitude} ${it.longitude}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
