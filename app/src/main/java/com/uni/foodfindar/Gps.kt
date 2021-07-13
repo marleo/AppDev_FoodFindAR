40ec0597678f90f00dd9a76eec41e03f50cc3ee5
tissy01 <musicraunjak22@gmail.com>
2021-07-13 13:55:41 +0200

    Auswahl für Settingsfilter

--- app/src/main/java/com/uni/foodfindar/Gps.kt
+++ app/src/main/java/com/uni/foodfindar/Gps.kt
@@ -2,7 +2,9 @@ package com.uni.foodfindar
 
 import android.app.Application
 
+
 class Gps : Application() {
     var longi = 0.0
     var lati = 0.0
+
 }
--- app/src/main/java/com/uni/foodfindar/Settings.kt
+++ app/src/main/java/com/uni/foodfindar/Settings.kt
@@ -9,6 +9,7 @@ import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -17,6 +18,7 @@ import com.google.android.gms.location.LocationServices
 class Settings : AppCompatActivity() {
     lateinit var setting: Button
     lateinit var location: Button
+    lateinit var filter: Button
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onBackPressed() {
@@ -30,12 +32,17 @@ class Settings : AppCompatActivity() {
 
         setting = findViewById(R.id.settings)
         location = findViewById(R.id.standort)
+        filter = findViewById(R.id.filter)
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         setting.setOnClickListener{
             finish()
             overridePendingTransition(0, 0)
+        }
+
+        filter.setOnClickListener{
+            showDialog()
         }
 
         findViewById<Button>(R.id.standort).setOnClickListener(){
@@ -56,11 +63,45 @@ class Settings : AppCompatActivity() {
         task.addOnSuccessListener {
             if(it != null){
                 Toast.makeText(applicationContext, "${it.latitude} ${it.longitude}", Toast.LENGTH_SHORT).show()
-                Gps().lati = it.latitude
-                Gps().longi = it.longitude
-                Toast.makeText(applicationContext, "${Gps().lati} ${Gps().longi}", Toast.LENGTH_SHORT).show()
             }
         }
 
     }
-}
+
+    private fun showDialog(){
+        lateinit var dialog:AlertDialog
+
+        val arrayLocations = arrayOf("Cafe","Restaurant", "Bar")
+        val arrayChecked = ArrayList<Int>()
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Choose your Preferences!")
+
+        builder.setMultiChoiceItems(arrayLocations, null) { dialog, which, isChecked ->
+
+            if(isChecked){
+                arrayChecked.add(which)
+            } else if (arrayChecked.contains(which)){
+                arrayChecked.remove(Integer.valueOf(which))
+            }
+
+
+        }
+
+        builder.setPositiveButton("OK"){ _, _ ->
+
+
+        }
+
+        builder.setNegativeButton("Cancel"){_, _ ->
+            dialog.dismiss()
+        }
+
+        dialog = builder.create()
+
+        dialog.show()
+
+        }
+    }
+
+
